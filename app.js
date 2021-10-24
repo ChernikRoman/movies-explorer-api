560fea2a72740d80b9929f66511cdfe5730fb9c7
Roman Chernik <chernik.r@yandex.ru>
2021-10-24 17:31:01 +0300

    Feat: добавить парсинг формата json и urlencoded

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
