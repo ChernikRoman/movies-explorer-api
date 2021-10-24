73ebf39bf53eff11ef91aed0181f48582cdc8a04
Roman Chernik <chernik.r@yandex.ru>
2021-10-24 15:52:02 +0300

    Feat: Настоить mongoose
    Подключить mongoose, настроить схемы и модели user, movie

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: (value) => validator.isEmail(value),
+    required: true,
+  },
+  password: {
+    type: String,
+    select: true,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
