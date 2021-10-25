fa78a69486959d519e945cbb4f1049e54d606d3d
Roman Chernik <chernik.r@yandex.ru>
2021-10-26 02:13:49 +0300

    Feat: Добавить контроллеры
    Добавлены контроллеры getMyUserData, patchMyUserData.
    Хеширование пароля вынесоно в утилиты

--- controllers/user.js
+++ controllers/user.js
@@ -1,37 +1,48 @@
-const bcrypt = require('bcrypt');
+const passwordHashing = require('../utils/passwordHashing');
 const User = require('../models/user');
 
-function createUser(req, res, next) {
+async function createUser(req, res, next) {
   const { name, email, password } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => {
-      User.create({
-        name,
-        email,
-        password: hash,
-      })
-        .then((data) => {
-          res.send({ name: data.name, email: data.email });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.send(err);
-        });
+  const hash = await passwordHashing(password);
+
+  User.create({
+    name,
+    email,
+    password: hash,
+  })
+    .then((data) => {
+      res.send({ name: data.name, email: data.email });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      res.send(err);
+    });
 }
 
 function getMyUserData(req, res, next) {
-  console.log(req, res);
-  const { userId } = req.cookies;
-  User.findById(userId);
-  next();
+  User.findById(req.userId)
+    .orFail(() => {
+      throw new Error('Not found');
+    })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => next(err));
 }
 
-function patchMyUserData(req, res, next) {
-  console.log(req, res);
-  next();
+async function patchMyUserData(req, res, next) {
+  const { name, email } = req.body;
+  let { password } = req.body;
+
+  if (password) {
+    password = await passwordHashing(req.body.password);
+  }
+  User.findByIdAndUpdate(req.userId, { name, email, password }, { new: true, runValidators: true }).select('+password')
+    .orFail(() => { throw new Error('Не найден'); })
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch(next);
 }
 
 module.exports = {
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const route = require('express').Router();
 const { getMyUserData, patchMyUserData } = require('../controllers/user');
 
-route.post('/me', getMyUserData);
 route.get('/me', getMyUserData);
 route.patch('/me', patchMyUserData);
 
